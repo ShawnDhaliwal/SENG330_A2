fa905e5634f48582e2a10e731040becba8811ae8
Shawn Dhaliwal <Dhaliwal@Shawns-MacBook-Pro.local>
2015-11-11 21:22:38 -0800

    Initial Commit

---
+++ SENG330_A2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SENG330_A2
+//
+//  Created by Shawn Singh Dhaliwal on 2015-11-11.
+//  Copyright (c) 2015 Shawn Singh Dhaliwal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
