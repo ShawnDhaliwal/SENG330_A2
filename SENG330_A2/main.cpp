4b7efb4ef489a80e5dd6a2aea7c04cc2e51d16d5
Shawn Dhaliwal <Dhaliwal@Shawns-MacBook-Pro.local>
2015-11-11 21:23:42 -0800

    Lets start assignment

--- SENG330_A2/main.cpp
+++ SENG330_A2/main.cpp
@@ -10,6 +10,6 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Lets start SENG330_A2 Assignment\n";
     return 0;
 }
