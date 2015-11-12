f942458f0a43457a4443eb4043ef620ca67f264c
Shawn Dhaliwal <Dhaliwal@Shawns-MacBook-Pro.local>
2015-11-11 21:28:53 -0800

    added index.cpp. Just testing stuff to get a better understanding of GitHub

---
+++ SENG330_A2/index.cpp
@@ -0,0 +1,9 @@
+//
+//  index.cpp
+//  
+//
+//  Created by Shawn Singh Dhaliwal on 2015-11-11.
+//
+//
+
+#include "index.h"
---
+++ SENG330_A2/index.h
@@ -0,0 +1,14 @@
+//
+//  index.h
+//  
+//
+//  Created by Shawn Singh Dhaliwal on 2015-11-11.
+//
+//
+
+#ifndef ____index__
+#define ____index__
+
+#include <stdio.h>
+
+#endif /* defined(____index__) */
